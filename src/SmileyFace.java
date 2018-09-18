3c363ea2e8ec4e210a7aee7c9f92a2c8610c9a51
JACOBSON <jjacobson20@student.gvsd.org>
2018-09-18 12:59:42 -0400

    fixed folders

---
+++ src/SmileyFace.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ * @author jeremy
+ * Smiley Face
+ */
+
+public class SmileyFace
+{
+	public static void main(String[] args)
+	{
+		System.out.println("\\\\\\\\\\\\\\");
+		System.out.println("| *  * |");
+		System.out.println("|  *   |");
+		System.out.println("| \\__/ |");
+		System.out.println("\\______/");
+	}
+
+	
+}
